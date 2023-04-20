53b15fcf4af185df20212e36c8599d98056dfe04
Khushal Jain <khushalj@outlook.com>
2023-04-21 01:07:38 +0530

    Update main.py
    Country code and phone number option side by side.
    Side by side of Rate Quoted and Total Quotation amount , added an option to select ₹ or $ from dropdown

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 
 # Create a function to save the data to a CSV file
-def save_data(name, phone, email, website, requirement, rate, timeline, status, amount):
+def save_data(name, phone, email, website, requirement, rate, currency, timeline, status, amount):
     data = pd.read_csv('leads.csv')
     new_data = pd.DataFrame({'Name': [name], 'Phone': [phone], 'Email': [email], 'Website': [website], 
-                             'Requirement': [requirement], 'Rate Quoted': [rate], 'Timeline': [timeline], 
+                             'Requirement': [requirement], 'Rate Quoted': [rate], 'Currency': [currency], 'Timeline': [timeline], 
                              'Deal Status': [status], 'Total Quotation Amount': [amount]})
     data = data.append(new_data, ignore_index=True)
     data.to_csv('leads.csv', index=False)
@@ -13,25 +13,40 @@ def save_data(name, phone, email, website, requirement, rate, timeline, status, 
 # Create a list of country codes
 country_codes = ['+1', '+44', '+61', '+91']
 
+# Create a list of currencies
+currencies = ['₹', '$']
+
 # Create a Streamlit app
 st.title('Lead Management System')
 st.write('Enter your lead information below:')
 
-# Create input fields for name, phone, email, website, requirement, rate, timeline, status, and amount
+# Create input fields for name, phone, email, website, requirement, rate, currency, timeline, status, and amount
 name = st.text_input('Name')
-phone = st.number_input('Phone', format='%d', step=1)
-country_code = st.selectbox('Country Code', country_codes)
+phone_col, currency_col = st.columns(2)
+with phone_col:
+    phone = st.number_input('Phone', format='%d', step=1)
+with currency_col:
+    currency = st.selectbox('Currency', currencies)
+country_code_col, phone_col = st.columns(2)
+with country_code_col:
+    country_code = st.selectbox('Country Code', country_codes)
+with phone_col:
+    phone_number = st.text_input('Phone Number')
 email = st.text_input('Email')
 website = st.text_input('Website')
 requirement = st.text_area('Requirement')
-rate = st.number_input('Rate Quoted')
+rate_col, amount_col = st.columns(2)
+with rate_col:
+    rate = st.number_input('Rate Quoted')
+with amount_col:
+    amount = st.number_input('Total Quotation Amount')
 timeline = st.text_input('Timeline')
 status = st.selectbox('Deal Status', ['Open', 'Close'])
-amount = st.number_input('Total Quotation Amount')
 
 # Add the lead to the CSV file when the submit button is clicked
 if st.button('Submit'):
-    phone_number = country_code + str(phone)
-    save_data(name, phone_number, email, website, requirement, rate, timeline, status, amount)
+    phone_number = country_code + phone_number
+    rate_quoted = str(rate) + currency
+    total_quotation_amount = str(amount) + currency
+    save_data(name, phone_number, email, website, requirement, rate_quoted, currency, timeline, status, total_quotation_amount)
     st.write('Data saved to CSV file!')
-
